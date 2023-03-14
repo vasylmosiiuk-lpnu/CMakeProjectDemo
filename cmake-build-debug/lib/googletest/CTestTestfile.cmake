0c840b4fd92741f11ec4e6bcbfbab98665eff3cf
Vasyl Mosiiuk <vasyl.v.mosiiuk@lpnu.ua>
2023-03-14 20:50:36 +0200

    removed cmake-build-debug

--- cmake-build-debug/lib/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/vasyl/Documents/OOP/CMakeProjectDemo/lib/googletest
-# Build directory: C:/Users/vasyl/Documents/OOP/CMakeProjectDemo/cmake-build-debug/lib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
